4683a654a1acf840a5644a6005722d6b707205c6
DivyeBhatnagar <divyebhatnagar784@gmail.com>
2026-02-27 19:30:42 +0530

    Day 7: Functions and recursion (factorial) in all languages

---
+++ java/Functions.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Functions {
+    
+    static int factorial(int n) {
+        if (n <= 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        System.out.println("Factorial of " + num + " = " + factorial(num));
+        
+        scanner.close();
+    }
+}
